b831889850b0be43065c33c4b7068a537f31ea97
stefanstefan2001 <scoartastefan@gmail.com>
2013-04-19 08:00:58 +0300

    Our Mod class, our first reference class, and our mcmod.info file

---
+++ mod_common/com/stef/mod/lib/Reference.java
@@ -0,0 +1,10 @@
+package com.stef.mod.lib;
+
+public class Reference {
+
+    public static final String MOD_ID ="MODL";
+    public static final String MOD_NAME = "MOD";
+    public static final String VERSION = "0.0.1";
+    
+    
+}
