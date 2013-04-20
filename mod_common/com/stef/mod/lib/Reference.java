bd4b9806f09ef1784e8df924b5ed95c944da60f2
stefanstefan2001 <scoartastefan@gmail.com>
2013-04-20 09:08:19 +0300

    Updated name

--- mod_common/com/stef/mod/lib/Reference.java
+++ mod_common/com/stef/mod/lib/Reference.java
@@ -2,8 +2,8 @@ package com.stef.mod.lib;
 
 public class Reference {
 
-    public static final String MOD_ID ="MODL";
-    public static final String MOD_NAME = "MOD";
+    public static final String MOD_ID ="Modding";
+    public static final String MOD_NAME = "Modding";
     public static final String VERSION = "0.0.1";
     
     
